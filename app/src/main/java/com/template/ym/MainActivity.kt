47c6d0bc65f90595e86d250453ee604539594c7f
Anika1d <roslyakov.vo@students.dvfu.ru>
2022-07-22 14:12:30 +1000

    initial project

---
+++ app/src/main/java/com/template/ym/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.template.ym
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.template.ym.ui.theme.YMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            YMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column() {
+        Text(text = "Hello $name!")
+        Image(painter = painterResource(id = R.drawable.ic_logo_120), contentDescription =null)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    YMTheme {
+        Greeting("Android")
+    }
+}
